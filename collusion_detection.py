47b904a158518d4ea81a66acc1524809f8bef897
Kevin Chen <kevin.chen.krc@gmail.com>
2022-02-03 17:02:19 -0500

    More collusion

--- collusion_detection.py
+++ collusion_detection.py
@@ -2,14 +2,17 @@
 # please use Python >=3.9
 
 from enum import Enum, auto
-# from keras.models import Sequential
-# from keras.layers import Dense
+from keras.models import Sequential
+from keras.layers import Dense
 import matplotlib.pyplot as plt
 import numpy as np
 import numpy.typing as npt
+import random
+import scipy.stats as scipystats
 from sklearn.metrics import mean_squared_error
 from sklearn.model_selection import train_test_split
 from sklearn.calibration import calibration_curve
+from sklearn.preprocessing import MinMaxScaler
 from statistics import NormalDist
 from typing import Optional, Union
 
@@ -17,7 +20,7 @@ from typing import Optional, Union
 class ReportStrategy(Enum):
     TRUE_BELIEFS = auto()
     COLLUSIVE_UP = auto()       # upvote
-    COLLSUIVE_ADV = auto()      #
+    COLLUSIVE_ADV = auto()      # upvote some, downvote others
 
 
 class SingleRecommenderDataGenerator:
@@ -50,22 +53,61 @@ class SingleRecommenderDataGenerator:
         '''returns outcomes and reports'''
         if type == ReportStrategy.TRUE_BELIEFS:
             return self.outcomes, self.true_beliefs
-        elif type == ReportStrategy.SINGLE_COLLUSIVE:
+        elif type == ReportStrategy.COLLUSIVE_UP:
             return self.outcomes, np.clip(self.true_beliefs + self.collusive_bias,
                                           0, 1)
         else:
             assert False, 'gen_reports(): invalid report strategy'
 
 
-class LendingDataGenerator:
-    def __init__(self, alpha: int = 6, beta: int = 4) -> None:
+class RecommenderDataGenerator:
+    def __init__(self,
+                 alpha: int = 6,
+                 beta: int = 4,
+                 n: int = 15,
+                 m: int = 15,
+                 belief_bias: float = 0,
+                 belief_sd: float = 0.05,
+                 collusive_bias: float = 0.15,
+                 collusive_frac: float = 0.1,
+                 ) -> None:
         self.alpha = alpha
         self.beta = beta
 
-    def draw_sample(self,
-                    size: Optional[Union[int, tuple[int]]] = None
-                    ) -> Union[float, npt.NDArray]:
-        return np.random.beta(self.alpha, self.beta, size)
+        self.alpha = alpha
+        self.beta = beta
+        self.n = n
+        self.m = m
+        self.collusive_bias = collusive_bias
+        self.collusive_frac = collusive_frac
+
+        self.true_probs = np.random.beta(alpha, beta, m)
+        self.true_beliefs = np.clip(np.tile(self.true_probs.transpose(), (self.n, 1)) +
+                                    np.random.normal(
+                                        belief_bias, belief_sd, (n, m)),
+                                    0, 1)
+        self.outcomes = np.random.binomial(1, self.true_probs)
+
+    def gen(self, type: ReportStrategy) -> tuple[npt.NDArray, npt.NDArray, Optional[int]]:
+        '''returns outcomes and reports'''
+        if type == ReportStrategy.TRUE_BELIEFS:
+            return self.outcomes, self.true_beliefs
+        elif type == ReportStrategy.COLLUSIVE_ADV:
+            assert self.n > 1
+            assert self.m > 2
+            collusive_i = random.randint(0, self.n - 1)
+            collusive_js = [random.randint(0, self.m - 1) for _ in range(5)]
+
+            reports = np.copy(self.true_beliefs)
+            reports[collusive_i, :] = np.clip(reports[collusive_i, :] +
+                                              np.array([self.collusive_bias
+                                                        if j in collusive_js
+                                                        else -1 * self.collusive_bias
+                                                        for j in range(self.m)]),
+                                              0, 1)
+            return self.outcomes, reports, collusive_i
+        else:
+            assert False, 'gen_reports(): invalid report strategy'
 
 
 class SingleClassificationModel:
@@ -87,7 +129,7 @@ def generate_true_predictions():
 
 def test_brier() -> None:
     for collusive_bias in [0.05, 0.1, 0.2, 0.3]:
-        n = 30
+        n = 15
         true_scores = []
         collusive_scores = []
 
@@ -97,7 +139,7 @@ def test_brier() -> None:
             true_scores.append(brier_score(outcomes, predictions))
 
             outcomes, predictions = SingleRecommenderDataGenerator(collusive_bias=collusive_bias).gen(
-                ReportStrategy.SINGLE_COLLUSIVE)
+                ReportStrategy.COLLUSIVE_UP)
             collusive_scores.append(brier_score(outcomes, predictions))
 
         plt.scatter(np.full(n, 0.4), true_scores, label='true reports')
@@ -119,8 +161,66 @@ def brier_score(outcomes: npt.ArrayLike, predictions: npt.ArrayLike) -> float:
     return mean_squared_error(outcomes, predictions)
 
 
+def test_creditworthiness() -> None:
+    N_ITERS = 100
+    n, m = 10, 15
+    data = []
+    output = []
+    for _ in range(N_ITERS):
+        _, reports, collusive_i = RecommenderDataGenerator(n=n, m=m).gen(
+            ReportStrategy.COLLUSIVE_ADV)
+        included_non_colluder = False
+        for i in range(n):
+            # remove effect of recommender i
+            avg_reports = np.mean(np.delete(reports, i, axis=0), axis=0)
+            params = scipystats.beta.fit(avg_reports, floc=0, fscale=1)
+            ksstat, pval = scipystats.kstest(reports[i], 'beta', args=params)
+            if i == collusive_i:
+                data.append([ksstat, pval])
+                output.append(i == collusive_i)
+            elif not included_non_colluder:
+                included_non_colluder = True
+                data.append([ksstat, pval])
+                output.append(i == collusive_i)
+
+    X = np.array(data)
+    y = np.array(output)
+
+    X = np.array(MinMaxScaler().fit_transform(X))
+
+    model = Sequential()
+    model.add(Dense(8, activation='relu', input_dim=2))
+    model.add(Dense(8, activation='relu'))
+    model.add(Dense(1, activation='sigmoid'))
+    model.compile(loss='binary_crossentropy',
+                  optimizer='adam',
+                  metrics=['accuracy'])
+    history = model.fit(X, y, validation_split=0.2,
+                        epochs=100, batch_size=10, verbose=0)
+    for layer in model.layers:
+        weights = layer.get_weights()
+    plt.plot(history.history['accuracy'])
+    plt.plot(history.history['val_accuracy'])
+    plt.title('model accuracy')
+    plt.ylabel('accuracy')
+    plt.xlabel('epoch')
+    plt.legend(['train', 'val'], loc='upper left')
+    plt.show()
+
+    plt.plot(history.history['loss'])
+    plt.plot(history.history['val_loss'])
+    plt.title('model loss')
+    plt.ylabel('loss')
+    plt.xlabel('epoch')
+    plt.legend(['train', 'val'], loc='upper left')
+    plt.show()
+
+    print(sum(y) / len(y))
+
+
 def main() -> None:
-    test_brier()
+    # test_brier()
+    test_creditworthiness()
 
 
 if __name__ == '__main__':
